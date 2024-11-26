e73b9fe421676b39c4d748d342bd3c857de9d350
Maria Fernanda Caetano <nanda2001caetano@gmail.com>
2024-11-26 09:55:36 -0300

    new branch created

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-# config/urls.py
-from django.contrib import admin 
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('books.urls')),
-    path('api/', include('api.urls')),
-]
